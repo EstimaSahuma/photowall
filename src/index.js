656083a88e3e32e981a0d8bc86b34b671b4b90d6
EstimaSahuma <estimaetilenoglicol@gmail.com>
2022-06-27 07:21:37 -0300

    Acess normal way

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const tasks = ['Angola', 'Cabo-Verde', 'Nigeria'];
+
+class List extends Component
+{
+  render()
+  {
+    return (<ol>
+        {tasks.map((task, index) => <li key={index}> {task} </li>)}
+      </ol>)
+  }
+}
+
+class Title extends Component
+{
+  render()
+  {
+    return <h1> Task List</h1>
+  }
+}
+
+class Main extends Component
+{
+  render()
+  {
+    return <div>
+      <Title/>
+      <List/>
+    </div>
+  }
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
